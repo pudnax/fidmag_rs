9e6dd7599753f3b923f6bcb9c4b248e6cecc2fea
Alex Komissarov <k.a.komissar@gmail.com>
2020-01-04 01:35:27 +0500

    Setup matherial

---
+++ benches/benchmarks.rs
@@ -0,0 +1,18 @@
+use std::fs;
+use std::io;
+use std::time::Duration;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use fidmag_rs;
+
+fn target_01(c: &mut Criterion) {
+    c.bench_function("f_func", |b| b.iter(|| {}));
+}
+
+criterion_group! {
+name = group;
+config = Criterion::default().warm_up_time(Duration::from_secs(5));
+targets = target_01
+}
+
+criterion_main!(group);
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
-fn main() {}
+use ndarray::prelude::*;
 
 use std::f64::consts::PI;
-const n: (f64, f64, f64) = (100., 25., 1.);
-const mu0: f64 = 4e-7 * PI;
-const gamma: f64 = 2.211e5;
-const ms: f64 = 8e5;
+const N: [usize; 3] = [100, 25, 1];
+static DEMAG_DIM: [usize; 4] = { [2 * N[0] - 1, 2 * N[1] - 1, 2 * N[2] - 1, 6] };
+const MU0: f64 = 4e-7 * PI;
+const GAMMA: f64 = 2.211e5;
+const MS: f64 = 8e5;
 const A: f64 = 1.3e-11;
-const alpha: f64 = 0.02;
+const ALPHA: f64 = 0.02;
 
-const eps: f64 = std::f64::EPSILON;
+const EPS: f64 = std::f64::EPSILON;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let buff = vec![0; DEMAG_DIM.iter().product()];
+    let n_demag = Array::from(buff);
+    let mut n_demag = n_demag.into_shape(DEMAG_DIM)?;
+
+    println!("{:?}", n_demag);
+    Ok(())
+}
+
+fn sest_n_demag<N, T>(demag: &mut Array<N, T>, c: usize, permute: usize, func: impl Fn()) {}
 
 fn f(p: [f64; 3]) -> f64 {
     let [x, y, z] = [p[0].abs(), p[1].abs(), p[2].abs()];
 
-    y / 2.0 * (z * z - x * x) * (y / ((x * x + z * z).sqrt() + eps)).asinh()
-        + z / 2.0 * (y * y - x * x) * (z / ((x * x + y * y).sqrt() + eps)).asinh()
-        - x * y * z * (y * z / (x * (x * x + y * y + z * z).sqrt() + eps)).atanh()
+    y / 2.0 * (z * z - x * x) * (y / ((x * x + z * z).sqrt() + EPS)).asinh()
+        + z / 2.0 * (y * y - x * x) * (z / ((x * x + y * y).sqrt() + EPS)).asinh()
+        - x * y * z * (y * z / (x * (x * x + y * y + z * z).sqrt() + EPS)).atanh()
         + 1.0 / 6.0 * (2. * x * x - y * y - z * z) * (x * x + y * y + z * z).sqrt()
 }
 
@@ -23,12 +35,12 @@ fn g(p: [f64; 3]) -> f64 {
     let [x, y, z] = p;
     let z = z.abs();
 
-    x * y * z * (z / ((x * z + y * y).sqrt() + eps)).asinh()
-        + y / 6.0 * (3.0 * z * z - y * y) * (x / ((y * y + z * z).sqrt() + eps)).asinh()
-        + x / 6.0 * (3.0 * z * z - x * z) * (y / ((x * z + z * z).sqrt() + eps)).asinh()
-        - z * z / 6.0 * (x * y / (z * (x * z + y * y + z * z).sqrt() + eps)).atan()
-        - z * y * y / 2.0 * (x * z / (y * (x * x + y * y + z * z).sqrt() + eps)).atan()
-        - z * x * x / 2.0 * (y * z / (x * (x * x + y * y + z * z).sqrt() + eps)).atan()
+    x * y * z * (z / ((x * z + y * y).sqrt() + EPS)).asinh()
+        + y / 6.0 * (3.0 * z * z - y * y) * (x / ((y * y + z * z).sqrt() + EPS)).asinh()
+        + x / 6.0 * (3.0 * z * z - x * z) * (y / ((x * z + z * z).sqrt() + EPS)).asinh()
+        - z * z / 6.0 * (x * y / (z * (x * z + y * y + z * z).sqrt() + EPS)).atan()
+        - z * y * y / 2.0 * (x * z / (y * (x * x + y * y + z * z).sqrt() + EPS)).atan()
+        - z * x * x / 2.0 * (y * z / (x * (x * x + y * y + z * z).sqrt() + EPS)).atan()
         - x * y * (x * x + y * y + z * z).sqrt() / 3.0
 }
 
