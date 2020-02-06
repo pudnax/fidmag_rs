3e695dbd48c59279dc73fd43172beda4f9ce16eb
Alex Komissarov <k.a.komissar@gmail.com>
2020-02-06 15:31:57 +0600

    Simple benchmark for fourier transform

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -5,23 +5,40 @@ use fidmag_rs;
 use rustfft::num_complex::Complex;
 use rustfft::num_traits::Zero;
 
+fn assert_eq_vecs(a: &[Complex<f64>], b: &[Complex<f64>]) {
+    for (a, b) in a.iter().zip(b) {
+        assert!((a - b).norm() < 0.1f64);
+    }
+}
+
 fn target_01(c: &mut Criterion) {
-    c.bench_function("f_func", |b| {
+    c.bench_function("fft", |b| {
         b.iter(|| {
-            let size = 500;
             let mut input: Vec<Complex<f64>> = vec![1., 2., 3., 4., 5., 6., 7., 8., 9.]
                 .into_iter()
                 .map(|x| Complex::new(x, 0.))
                 .collect();
-            let mut output = vec![Zero::zero(); size];
+            let mut output = vec![Zero::zero(); 9];
             fidmag_rs::fft::fft(&mut input, &mut output);
+            let expected = [
+                Complex::new(45.0, 0.),
+                Complex::new(-4.5, 12.363_648_39),
+                Complex::new(-4.5, 5.362_891_17),
+                Complex::new(-4.5, 2.598_076_21),
+                Complex::new(-4.5, 0.793_471_41),
+                Complex::new(-4.5, -0.793_471_41),
+                Complex::new(-4.5, -2.598_076_21),
+                Complex::new(-4.5, -5.362_891_17),
+                Complex::new(-4.5, -12.363_648_39),
+            ];
+            assert_eq_vecs(&expected, &output);
         })
     });
 }
 
 criterion_group! {
 name = group;
-config = Criterion::default().warm_up_time(Duration::from_secs(3));
+config = Criterion::default().warm_up_time(Duration::from_secs(5));
 targets = target_01
 }
 
